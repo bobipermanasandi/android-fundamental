1835c1bca0b1e261562bf868a44e735d18e9e62b
Bobi Permana Sandi <bobipermana17@gmail.com>
2024-04-14 16:35:03 +0700

    delete submission

--- [Submission] github-user/app/src/main/java/id/bobipermanasandi/githubuser/data/ResultData.kt
+++
@@ -1,7 +0,0 @@
-package id.bobipermanasandi.githubuser.data
-
-sealed class ResultData<out R> private constructor() {
-    data class Success<out T>(val data: T) : ResultData<T>()
-    data class Error(val error: String) : ResultData<Nothing>()
-    object Loading : ResultData<Nothing>()
-}
